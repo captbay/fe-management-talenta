37dca4260c465a40ae4760ddd557f591edb5a9ba
I Putu Agestya Pramana <agespramana9@gmail.com>
2023-11-21 22:58:51 +0700

    hosting

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import ax from "axios";
 
 export const axios = ax.create({
-  baseURL: "http://management-talenta.test/api",
-  // baseURL: "https://management-talenta.ppcdeveloper.com/api",
+  // baseURL: "http://management-talenta.test/api",
+  baseURL: "https://management-talenta.ppcdeveloper.com/api",
 });
